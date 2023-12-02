ea477860d196c3ec324318cacb5684570fb48928
kestulka <kgirdiunas0@gmail.com>
2023-12-02 13:48:59 +0200

    ajajaj

--- pratybos.js
+++ pratybos.js
@@ -1,3 +1,5 @@
+"use strict";
+
 // 61 skaidre
 
 // 1:
@@ -50,8 +52,86 @@
 
 // 3:
 
-const sakinys = "labas rytas gal norite rytines kavutes ar baltos degtines ?";
-const zodziai = sakiniuMasyvas.split(" "); // is sakinio sukuria masyva
+// const sakinys = "labas rytas gal norite rytines kavutes ar baltos degtines ?";
+// const masyvas = sakinys.split(" "); // is sakinio sukuria masyva
 
-const lyginioIndeksoZodziai = [];
-const nelyginioIndeksoZodziai = [];
+// const lyginiaiZodziai = masyvas.filter((word, index) => index % 2 !== 0);
+// const nelyginiaiZodziai = masyvas.filter((word, index) => index % 2 === 0);
+
+// const lyginiuZodziuSakinys = lyginiaiZodziai.join(" ");
+// const nelyginiuZodziuSakinys = nelyginiaiZodziai.join(" ");
+
+// console.log(masyvas);
+// console.log(lyginiaiZodziai);
+// console.log(nelyginiaiZodziai);
+// console.log(lyginiuZodziuSakinys);
+// console.log(nelyginiuZodziuSakinys);
+
+// 4.
+
+const masyvas = [5, 3, 2, 8, 13, 16, 18, 23, 27, 30];
+
+// rasti maziausia metodas #1:
+
+// let maziausias = 0;
+// let minValue = masyvas[0];
+
+// for (let i = 1; i < masyvas.length; i++) {
+//   if (masyvas[i] < minValue) {
+//     minValue = masyvas[i];
+//     maziausias = i;
+//   }
+// }
+// console.log(maziausias);
+
+// rasti maziausia metodas #2:
+
+const maziausias = masyvas.reduce((maziausias, value, currentIndex) => {
+  if (value < masyvas[maziausias]) {
+    return currentIndex;
+  }
+  return maziausias;
+}, 0);
+
+console.log(maziausias);
+
+// pasalinti 3 nuo maziausias metodas #1:
+
+// const masyvasPasalintas = masyvas
+//   .slice(0, maziausias)
+//   .concat(masyvas.slice(maziausias + 4));
+
+// const pasalintiElementai = masyvas.slice(maziausias, maziausias + 4);
+
+// console.log(masyvasPasalintas);
+// console.log(pasalintiElementai);
+
+// pasalinti 3 nuo maziausias metodas #2:
+
+// const masyvasPasalintas = masyvas.filter(
+//   (_, index) => index < maziausias || index >= maziausias + 4,
+// );
+
+// be arrow:
+
+// const masyvasPasalintas = masyvas.filter(function (_, index) {
+//   return index < maziausias || index >= maziausias + 4;
+// });
+
+// console.log(masyvasPasalintas);
+
+// pasalinti 3 nuo maziausias metodas #3:
+
+// if (maziausias >= 0 && maziausias + 2 < masyvas.length) {
+//   const masyvasPasalintas = [...masyvas]; // sukuriamas naujas masyvas su kito masyvo duomenimis
+//   masyvasPasalintas.splice(maziausias, 4);
+//   console.log(masyvasPasalintas);
+// } else {
+//   console.log("miau");
+// }
+
+// arba
+
+const masyvasPasalintas = [...masyvas];
+masyvasPasalintas.splice(maziausias, 4);
+console.log(masyvasPasalintas);
